8687a937edbd86d7a16a6ae426c6a21e1c5f2976
BasJansw <bas.jansweijer@uva.student.nl>
2024-04-22 21:47:31 +0200

    add/improve output/input handeling of protocols

--- SMPCbox/AbstractProtocol.py
+++ SMPCbox/AbstractProtocol.py
@@ -16,6 +16,10 @@ class AbstractProtocol (ABC):
     # A list storing all the protocol steps. The ProtocolStep class is used to store the description of what happens in each step
     protocol_steps: list[Union[ProtocolStep, ProtocolSubroutine]] = []
 
+    # protocol_output
+    protocol_output: dict[str, dict[str, Any]] = None
+
+
     def __init__(self):
         self.running_party = None
 
@@ -45,11 +49,22 @@ class AbstractProtocol (ABC):
     
         Note that in the case that the protocol is run distributedly the party running localy should also be specified with the
         set_running_party method.
+
+        WARNING: Any data, such as input set with the set_input method, which is already stored in the existing ProtocolParty classes
+        is lost once this method is called.
         """
+
+        party_names = set(party.name for party in role_assignments.values())
+        if len(party_names) != len(role_assignments.values()):
+            raise Exception("Make sure each ProtocolParty partaking in a protocol has a unique name!")
 
         if set(role_assignments.keys()) != set(self.get_party_roles()):
             raise Exception("A ProtocolParty instance should be provided for every role in the protocol when calling set_protocol_parties.")
         self.parties = role_assignments
+    
+    def check_role_exists(self, role: str):
+        if not role in self.get_party_roles():
+            raise Exception(f"The role \"{role}\" does not exist in the protocol\"{self.protocol_name}\"")
 
     def set_running_party(self, role: str, party: ProtocolParty):
         """
@@ -57,10 +72,8 @@ class AbstractProtocol (ABC):
         To do this a ProtocolParty instance must be provided and the role that the party should have must be specified.
         The available roles can be retrieved with the get_party_roles method.
         """
-        if not role in self.get_party_roles():
-            raise Exception(f"The role \"{role}\" does not exist in the protocol\"{self.protocol_name}\"")
+        self.check_role_exists(role)
         self.running_party = party.name
-        self.parties[role] = party
 
     def is_local_party(self, party: ProtocolParty) -> bool:
         """
@@ -114,7 +127,6 @@ class AbstractProtocol (ABC):
 
         # add the local computation
         self.protocol_steps[-1].add_opperation(LocalComputation(computing_party, computed_var_names, description))
-
     
     def add_protocol_step(self, step_name: str = None):
         """
@@ -176,6 +188,56 @@ class AbstractProtocol (ABC):
         """
         pass
 
+    def set_input(self, inputs: dict[str, dict[str, Any]]):
+        """
+        Sets the inputs for the protocols (all inputs specified by get_expected_input) should be given
+        If set_running_party has been called only the input for that party needs to be given
+        If the protocol is not run distributed then the inputs for all the parties should be provided.
+        
+        This method also checks wether the provided input is correct according to the get_expected_input method
+
+        WARNING: This method uses the ProtocolParty classes to store the provided input. Calling set_protocol_parties 
+        after a call to this method thus erases the input.
+        """
+        expected_vars = self.get_expected_input()
+        for role in inputs.keys():
+            self.check_role_exists(role)
+
+            # check wether the inputs are provided correctly for each role
+            if set(expected_vars[role]) != set(inputs[role].keys()):
+                expected_set = set(expected_vars[role])
+                given_set = set(inputs[role].keys())
+                raise Exception(f"""The inputs for the role \"{role}\" in the protocol \"{self.protocol_name}\" are incorrect.\n
+                                    Missing variables {expected_set.difference(given_set)}\n
+                                    Provided non existent input variables {given_set.difference(expected_set)}""")
+            
+            # Set the inputs
+            for var in inputs[role].keys():
+                self.parties[role].set_local_variable(var, inputs[role][var])
+    
+    @abstractmethod
+    def output_variables(self) -> dict[str, list[str]]:
+        """
+        A protocol must define what output variables are produced by the protocol.
+        The dictionary should contain a mapping from each role who produces output to a list of variables
+        that are produced. These variables should be local variables of the ProtocolParty assigned to that role
+        by the end of the protocol execution
+        """
+        pass
+
+    def get_output(self) -> dict[str, dict[str, Any]]:
+        """
+        Using the defined output variables from the output_variables method this method returns all the values of the output variables
+        """
+        output = {}
+        for role in self.output_variables().keys():
+            self.check_role_exists(role)
+            output[role] = {}
+            for var in self.output_variables()[role]:
+               output[role][var] = self.parties[role].get_variable(var)
+
+        return output
+
 
     def announce_globals(self, announcing_party: ProtocolParty, variables: Union[str, list[str]]):
         """
@@ -198,21 +260,48 @@ class AbstractProtocol (ABC):
         self.protocol_steps[-1].add_opperation(AnnounceGlobals(announcing_party, variables))
 
     
-    def run_subroutine_protocol(self, protocol: Type['AbstractProtocol'], role_assignments: dict[str, ProtocolParty], inputs: dict[str, dict[str, Any]]):
+    def run_subroutine_protocol(self, protocol: Type['AbstractProtocol'], role_assignments: dict[str, ProtocolParty], inputs: dict[str, list[str]]):
         """
         Runs the provided protocol as part of the current protocol. Appart from the protocol class (not instance), there are two required arguments:
         role_assignments: A dictionary which should map every role of the protocol being run to an existing ProtocolParty.
                           The roles can be retreived with the get_party_roles method of the protocol class.
-        inputs: This maps the inputs of every role in the protocol being run. The dictionary should contain all the inputs as specified by the get_expected_input method
+        inputs: Defines the variables for each role in the protocol which should be used as input. Note that this maps from roles to input variables not
+                from party names to input variables.
             
         Note that the keys in the inputs and role_assignments dictionaries should be roles specified in the get_party_roles method of the provided protocol
         """
+
+        p = protocol()
+
+        # before calling start_subroutine_protocol on the parties
+        # we first gather the provided variables from the parties to avoid namespace issues.
+        input_values = {}
+        # used for visualisation
+        input_var_mapping = {}
+        for role in inputs.keys():
+            self.check_role_exists(role)
+            party = role_assignments[role]
+
+            if not self.is_local_party(party):
+                # No need to set the input of non local parties
+                continue
+
+            # get the values for each of the input variables.
+            input_values[role] = {}
+            input_var_mapping[role] = {}
+            expected_input_vars = p.get_expected_input()[role]
+            for input_var_name, provided_var in zip(expected_input_vars, inputs[role]):
+                # Set the input variable
+                if (self.is_local_party(party)):
+                    input_values[input_var_name] = role_assignments[role].get_variable(provided_var)
+                    input_var_mapping[input_var_name] = provided_var
+
+            party = role_assignments[role]
 
         # comunicate to the participating parties that they are entering a subroutine
         for party in role_assignments.values():
             party.start_subroutine_protocol(protocol.protocol_name)
 
-        p = protocol()
         p.set_protocol_parties(role_assignments)
 
         # Comunicate to the protocol wether a certain party is running the protocol localy
@@ -222,8 +311,10 @@ class AbstractProtocol (ABC):
                 if self.running_party == party.name:
                     protocol.set_running_party(role, party)
         
-        # set the inputs
-        p.set_input(inputs)
+        
+        # set the constructed input_values
+        p.set_input(input_values)
+
         # run the protocol
         p()
 
@@ -231,31 +322,8 @@ class AbstractProtocol (ABC):
         for party in role_assignments.values():
             party.end_subroutine_protocol()
         
-        self.protocol_steps.append()
+        self.protocol_steps.append(ProtocolSubroutine(protocol, role_assignments, input_var_mapping))
 
-    def set_input(self, inputs: dict[str, dict[str, Any]]):
-        """
-        Sets the inputs for the protocols (all inputs specified by get_expected_input) should be given
-        If set_running_party has been called only the input for that party needs to be given
-        If the protocol is not run distributed then the inputs for all the parties should be provided.
-        
-        This method also checks wether the provided input is correct according to the get_expected_input method
-        """
-
-
-        expected_vars = self.get_expected_input()
-        for role in inputs.keys():
-            # check wether the inputs are provided correctly for each role
-            if set(expected_vars[role]) != set(inputs[role].keys()):
-                expected_set = set(expected_vars[role])
-                given_set = set(inputs[role].keys())
-                raise Exception(f"""The inputs for the role \"{role}\" in the protocol \"{self.protocol_name}\" are incorrect.\n
-                                    Missing variables {expected_set.difference(given_set)}\n
-                                    Provided non existent input variables {given_set.difference(expected_set)}""")
-            
-            # Set the inputs
-            for var in inputs[role].keys():
-                self.parties[role].set_local_variable(var, inputs[role][var])
     
     def terminate_protocol(self):
         """
